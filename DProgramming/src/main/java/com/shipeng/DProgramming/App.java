997bf75157de3b953b69dbee1412b87c5be78c99
Shipeng Xu <sxu@Shipeng-Xu.local>
2017-01-12 18:07:50 -0800

    added new programs for Recursion and DProgramming.

---
+++ DProgramming/src/main/java/com/shipeng/DProgramming/App.java
@@ -0,0 +1,13 @@
+package com.shipeng.DProgramming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ PasswordGenerator/src/main/java/com/shipeng/PasswordGenerator/PasswordGenerator.java
@@ -0,0 +1,22 @@
+package com.shipeng.PasswordGenerator;
+
+public class PasswordGenerator {
+
+	private String[] char_array = null;
+	
+	public PasswordGenerator() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	} // end main
+	
+	private void populateCharArray(String[] char_array) {
+		
+	}
+	
+	
+} // end PasswordGenerator
+
+
