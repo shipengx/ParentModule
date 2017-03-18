13a032819b6b883fe2750fd60d47fb8777e5798e
Shipeng Xu <sxu@Shipeng-Xu.local>
2017-03-18 01:18:52 -0700

    added new problems.

--- DataStructure/src/main/java/aa/ValidAnagrams.java
+++ DataStructure/src/main/java/aa/ValidAnagrams.java
@@ -1,5 +1,47 @@
 package aa;
 
+/*
+ * Valid Anagram
+ * 
+ * Given two strings s and t, write a function to determine if t is an anagram of s.
+ * 
+ * For example,
+ * s = "anagram", t="nagaram", return true.
+ * s = "rat", t = "car", return false.
+ * 
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ * 
+ * Follow up:
+ * what if the input contains unicode character? 
+ * How would you adapt your solution to such case?
+ * 
+ */
+
 public class ValidAnagrams {
+
+	public boolean isAnagram(String s, String t) {
+		if (s == null || t == null) {
+			return false;
+		}
+
+		if (s.length() != t.length()) {
+			return false;
+		}
+
+		int[] array = new int[26];
+		for (int i = 0; i < s.length(); i++) {
+			array[s.charAt(i) - 'a']++;
+			array[t.charAt(i) - 'a']--;
+		}
+
+		for (int i : array) {
+			if (i != 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 
 }
