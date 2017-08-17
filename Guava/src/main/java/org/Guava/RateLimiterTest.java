1986a03ed2342d526bd21179c8681d1068269162
Shipeng Xu <sxu@Shipeng-Xu.local>
2017-08-17 11:42:19 -0700

    added a little change for Guava RateLimiter example.

--- Guava/src/main/java/org/Guava/RateLimiterTest.java
+++ Guava/src/main/java/org/Guava/RateLimiterTest.java
@@ -1,22 +1,32 @@
 package org.Guava;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import com.google.common.util.concurrent.RateLimiter;
 
 public class RateLimiterTest {
 
 	public static void main(String[] args) {
-		RateLimiter rateLimiter = RateLimiter.create(2.0);
+		RateLimiter rateLimiter = RateLimiter.create(1);
 		int i = 3;
 		while (i > 2) {
 			performOperation(rateLimiter);
+			try {
+			    Thread.sleep(333);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 		}
 		
 	}
 	
 	private static void performOperation(RateLimiter limiter) {
-		limiter.acquire();
-		System.out.println(new Date() + ": Beep");
+		//limiter.acquire();
+		if (limiter.tryAcquire()) {
+			System.out.println(new Date() + ": Beep");
+		}else {
+			System.out.println("QPSThrottled.");
+		}
 	}
 }
