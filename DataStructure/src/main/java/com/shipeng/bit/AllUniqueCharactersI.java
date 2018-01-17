aa5777fe0d8387023a805e76b70d8cfd13e2569c
Shipeng Xu <sxu@Shipeng-Xu.local>
2018-01-16 23:22:20 -0800

    added new problems.

--- DataStructure/src/main/java/com/shipeng/bit/AllUniqueCharactersI.java
+++ DataStructure/src/main/java/com/shipeng/bit/AllUniqueCharactersI.java
@@ -1,5 +1,32 @@
 package com.shipeng.bit;
 
+/*
+ * All Unique Characters I
+ * 
+ * Determine if the characters of a given String are all unique.
+ * 
+ * Assumptions:
+ *     the only set of possible characters used in the string are 'a' - 'z', the 26 lowercase letters.
+ *     the given string is not null.
+ *     
+ * Examples:
+ *     the characters used in "abcd" are unique
+ *     the characters used in "aba" are not unique
+ * 
+ */
+
 public class AllUniqueCharactersI {
 
+	public boolean allUnique(String word) {
+		// an integer is good enough to store (26 lowercase letters)
+		int result = 0;
+		char[] array = word.toCharArray();
+		for (char c : array) {
+			if (((result >> c) & 1) == 1) {
+				return false;
+			}
+			result |= 1 << c;
+		}
+		return true;
+	}
 }
--- DataStructure/src/main/java/com/shipeng/bit/AllUniqueCharactersII.java
+++ DataStructure/src/main/java/com/shipeng/bit/AllUniqueCharactersII.java
@@ -1,5 +1,37 @@
 package com.shipeng.bit;
 
+/*
+ * All unique characters II
+ * Determine if the characters of a given string are all unique.
+ * 
+ * Assumptions:
+ *     We are using ASCII charset, the value of valid characters are from 0 to 255
+ *     The given string is not null
+ * Examples:
+ *     all the characters in "abA+\8" are unique
+ *     "abA+\a88" contains duplicate characters
+ * 
+ */
+
 public class AllUniqueCharactersII {
+
+	public boolean allUnique(String word) {
+		// each int value can represent 32 bits, so we need 8 ints to represent 256
+		// bits.
+
+		int[] vec = new int[8];
+		char[] array = word.toCharArray();
+		for (char c : array) {
+			// for a value c in the range of 0-255,
+			// it is actually mapped to the int value at c/32 as index,
+			// and the c%32th bit of the int value.
+			if ((vec[c / 32] >>> (c % 32) & 1) != 0) {
+				return false;
+			}
+			vec[c / 32] |= 1 << (c % 32);
+		}
+		return true;
+
+	}
 
 }
