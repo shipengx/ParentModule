69c0c17ee390a7961aac5a9d9139595bfe86fb51
Shipeng Xu <sxu@Shipeng-Xu.local>
2018-02-11 22:08:33 -0800

    added new problems.

--- DataStructure/src/main/java/com/ti/class26/MajorityNumberI.java
+++ DataStructure/src/main/java/com/ti/class26/MajorityNumberI.java
@@ -1,5 +1,37 @@
 package com.ti.class26;
 
+/*
+ * Majority Number I
+ * 
+ * Given an integer array of length L, find the number that occurs more than 0.5 * L times.
+ * 
+ * Assumptions:
+ *     The given array is not null or empty
+ *     It is guaranteed there exists such a majority number
+ *     
+ * Examples
+ *     A = { 1, 2, 1, 2, 1}, return 1
+ * 
+ */
+
 public class MajorityNumberI {
+
+	public int majority(int[] array) {
+		// write your solution here
+		// Assumptions: array is not null and is not empty
+		// majority number guarantees to exist.
+		int candidate = array[0];
+		int count = 1;
+		for (int i = 0; i < array.length; i++) {
+			if (count == 0) {
+				count++;
+			} else if (candidate == array[i]) {
+				count++;
+			} else {
+				count--;
+			}
+		}
+		return candidate;
+	}
 
 }
--- DataStructure/src/main/java/com/ti/class26/MaximumValueOfSizeKSlidingWindow.java
+++ DataStructure/src/main/java/com/ti/class26/MaximumValueOfSizeKSlidingWindow.java
@@ -1,5 +1,56 @@
 package com.ti.class26;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * Maximum values of size k sliding window
+ * 
+ * given an integer array A and a sliding window of size k,
+ * find the maximum value of each window as it slides from left to right.
+ * 
+ * Assumptions:
+ *     The given array is not null and is not empty
+ *     K >= 1, K <= A.length
+ *     
+ * Examples:
+ *     A = { 1, 2, 3, 2, 4, 2, 1 }, K = 3, the windows are 
+ *     {{1,2,3}, {2,3,2}, {3,2,4}, {2,4,2}, {4,2,1}},
+ *     and the maximum values of each K-sized sliding window are [3,3,4,4,4]
+ * 
+ */
+
 public class MaximumValueOfSizeKSlidingWindow {
+
+	public List<Integer> maxWindows(int[] array, int k) {
+		// Assumptions: array is not null or empty,
+		// K >= 1 and K <= a.length
+		List<Integer> max = new ArrayList<Integer>();
+		// use a descending deque to solve this problem,
+		// we store the index instead of the actual value in the deque,
+		// and we make sure:
+		// 1. the deque only contains index in the current sliding window,
+		// 2. for any index, the previous index with smaller value is discarded from the
+		// deque.
+		Deque<Integer> deque = new LinkedList<Integer>();
+		for (int i = 0; i < array.length; i++) {
+			// discard any index with smaller value than index i
+			while (!deque.isEmpty() && array[deque.peekLast()] <= array[i]) {
+				deque.pollLast();
+			}
+			// it is possible the head element is out of the current
+			// sliding window so we might need to discard it as well.
+			if (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+				deque.pollFirst();
+			}
+			deque.offerLast(i);
+			if (i >= k - 1) {
+				max.add(array[deque.peekFirst()]);
+			}
+		}
+		return max;
+	}
 
 }
