5f85228a0ac561be73e5825770eba078018b167e
Shipeng Xu <shipengx@shipengs-mbp.attlocal.net>
2019-09-19 17:53:49 -0700

    added new changes.

--- DataStructure/src/main/java/sf/ValidParentheses.java
+++ DataStructure/src/main/java/sf/ValidParentheses.java
@@ -1,4 +1,79 @@
 package sf;
 
+import java.util.Stack;
+
+/**
+ * Valid Parentheses
+ * Given a String containing just the characters '(', ')', '{', '}', '[', ']',
+ * determine if the input string is valid.
+ *
+ * An input string is valid if:
+ * 1. open brackets must be closed by the same type of brackets.
+ * 2. open brackets must be closed in the correct order.
+ *
+ * Note that an empty string is also considered valid.
+ *
+ * Example 1:
+ * Input: "()"
+ * Output: true
+ *
+ * Example 2:
+ * Input: "()[]{}"
+ * Output: true
+ *
+ * Example 3:
+ * Input: "(]"
+ * Output: false
+ *
+ *
+ * Example 4:
+ * Input: "([)]"
+ * Output: false
+ *
+ * Example 5:
+ * Input: "{[]}"
+ * Output: true
+ *
+ */
+
 public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] array = s.toCharArray();
+
+        for (int i = 0; i < array.length; i++) {
+            char current = array[i];
+            if (current == '(' || current == '[' || current == '{') {
+                stack.push(current);
+            } else if (current == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (current == ']') {
+                if (!stack.isEmpty() && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (current == '}') {
+                if (!stack.isEmpty() && stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                continue;
+            }
+        } // end for loop
+
+        return stack.isEmpty();
+    }
+
+
 }
