3bff91dd0f2b2f0c5b11e12722aca860a5a93350
sam-adara <shipeng.xu@adara.com>
2019-09-23 16:03:11 -0700

    added new changes.

---
+++ SpringBootExample/build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+#
+
+
+docker build -t myspringbootapp .
+docker run -p 8080:8080 myspringbootapp
+
+
+
+# Start minikube
+minikube start
+
+# Set docker env
+eval $(minikube docker-env)
+
+# Build image
+docker build -t myspringbootapp .
+
+# Run in minikube
+kubectl run myspringbootapp --image=myspringbootapp:latest --image-pull-policy=Never --port=8080
+
+# Check that it's running
+kubectl get pods
+
+
+kubectl expose deployment myspringbootapp --type=NodePort
+
+curl -X GET http://$(minikube ip):30486/
+
+
+
